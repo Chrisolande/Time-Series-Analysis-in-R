bc41f07bf424b6848c514da275eaa06d3557b52a
Chris Olande <6828.2022@students.ku.ac.ke>
2025-08-01 19:03:59 +0300

    Adds ARIMA, SARIMA modeling and forecasting examples
    The commit adds several examples demonstrating the application of ARIMA and SARIMA models
    for time series analysis and forecasting.  Demonstrates how to identify and apply these models using practical datasets, and how to interpret the results and evaluate their performance.

--- ARIMA.r
+++ ARIMA.r
@@ -9,7 +9,9 @@ librarian::shelf(
   TSstudio,
   plotly,
   ggfortify,
-  gridExtra
+  gridExtra,
+  forecast,
+  tidyverse
 )
 
 # %%
@@ -93,28 +95,28 @@ p2 <- plot_ly()
 
 for (i in 1:20) {
   rw <- arima.sim(model = list(order = c(0, 1, 0)), n = 500)
-  p1 <- p1 %>% add_lines(x = time(rw), y = as.numeric(rw))
-  p2 <- p2 %>% add_lines(x = time(diff(rw)), y = as.numeric(diff(rw)))
+  p1 <- p1 |> add_lines(x = time(rw), y = as.numeric(rw))
+  p2 <- p2 |> add_lines(x = time(diff(rw)), y = as.numeric(diff(rw)))
 }
 
 # %%
 # Plot random walk simulations
-p1 %>%
+p1 |>
   layout(
     title = "Simulate Random Walk",
     yaxis = list(title = "Value"),
     xaxis = list(title = "Index")
-  ) %>%
+  ) |>
   hide_legend()
 
 # %%
 # Plot differenced random walk simulations
-p2 %>%
+p2 |>
   layout(
     title = "Simulate Random Walk with First-Order Differencing",
     yaxis = list(title = "Value"),
     xaxis = list(title = "Index")
-  ) %>%
+  ) |>
   hide_legend()
 
 # The AR Process
@@ -176,3 +178,190 @@ p1 <- autoplot(acf(arma, plot = FALSE))
 p2 <- autoplot(pacf(arma, plot = FALSE))
 
 grid.arrange(p1, p2, ncol = 2)
+
+# Forecasting with AR, MA, ARMA
+# %%
+ar_fc <- forecast(md_ar, h = 100)
+plot_forecast(
+  ar_fc,
+  title = "Forecast AR(2) Model",
+  Ytitle = "Value",
+  Xtitle = "Year"
+)
+
+# The ARIMA Model
+# Handles non-stationary data, something ARMA can't do
+# The Integrating process is simply differencing the series with the lags
+
+# Identifying the ARIMA Model
+# 1. Identify the degree of differencing that is required to transfer the series into a
+#    stationary state
+# 2. Identify the ARMA process (or AR and MA processes)
+# %%
+data("Coffee_Prices")
+robusta_price <- window(Coffee_Prices[, 1], start = c(2000, 1))
+
+# %%
+ts_plot(
+  robusta_price,
+  title = "The Robusta Coffee Monthly Prices",
+  Ytitle = "Price in USD",
+  Xtitle = "Year"
+)
+
+# %%
+autoplot(acf(robusta_price, plot = FALSE))
+# The correlation of the series with its lags is slowly decaying over time in a linear manner.
+# Removing both the series trend and correlation between the series and its lags can be done
+# by differencing the series
+
+# %%
+robusta_price_d1 <- diff(robusta_price)
+
+# %%
+p1 <- autoplot(acf(robusta_price_d1, plot = FALSE))
+p2 <- autoplot(pacf(robusta_price_d1, plot = FALSE))
+
+grid.arrange(p1, p2, ncol = 2)
+
+# The ACF and PACF plots of the first difference of the series indicate that an AR(1) process
+# is appropriate to use on the differenced series since the ACF is tailing off and the PACF cuts
+# on the first lag
+
+# %%
+robusta_md <- arima(robusta_price, order = c(1, 1, 0))
+summary(robusta_md)
+
+# %%
+checkresiduals(robusta_md)
+
+# SARIMA
+# The ARIMA variant for seasonal data, it utilizes the seasonal lags by adding:
+# 1. The seasonal AR Process (P)
+# 2. The Seasonal MA Process (Q)
+# 3. The Seasonal Integration Process (D)
+# It now goes on to take the form: SARIMA(p, d, q) X (P, D, Q)_s
+
+# %%
+data(USgas)
+ts_plot(
+  USgas,
+  title = "US Monthly Natural Gas Consumption",
+  Ytitle = "Billion Cubic Feet",
+  Xtitle = "Year"
+)
+
+# %%
+usgas_split <- ts_split(USgas, sample.out = 12)
+train <- usgas_split$train
+test <- usgas_split$test
+
+# %%
+p1 <- autoplot(acf(train, lag.max = 60, plot = FALSE))
+p2 <- autoplot(pacf(train, lag.max = 60, plot = FALSE))
+grid.arrange(p1, p2, ncol = 2)
+# The preceding ACF plot indicates that the series has a strong correlation with both the
+# seasonal and non-seasonal lags. The linear decay of the seasonal lags indicates
+# that the series is not stationary and that seasonal differencing is required.
+
+# %%
+# Difference the series and plot to see if the stationarity condition is met
+usgas_d12 <- diff(train, 12)
+ts_plot(
+  usgas_d12,
+  title = "US Monthly Natural Gas Consumption - First Seasonal Difference",
+  Ytitle = "Billion Cubic Feet (First Difference)",
+  Xtitle = "Year"
+)
+# The trend is removed, the variation is not, difference again:
+
+# %%
+usgas_d12_1 <- diff(diff(usgas_d12, 1))
+ts_plot(
+  usgas_d12_1,
+  title = "US Monthly Natural Gas Consumption - First Seasonal and Non-Seasonal Differencing",
+  Ytitle = "Billion Cubic Feet (Difference)",
+  Xtitle = "Year"
+)
+# The series is now stabilized
+
+# %%
+p1 <- autoplot(acf(usgas_d12_1, lag.max = 60, plot = FALSE))
+p2 <- autoplot(pacf(usgas_d12_1, lag.max = 60, plot = FALSE))
+grid.arrange(p1, p2, ncol = 2)
+
+# Both the non-seasonal and seasonal lags (in both plots) are tailing off. Hence, we can
+# conclude that after we difference the series and transform them into a stationary state,
+# we should apply an ARMA process for both the seasonal and non-seasonal components of
+# the SARIMA model.
+
+# %%
+# Grid search for optimal SARIMA parameters
+p <- q <- P <- Q <- 0:2
+
+# %%
+arima_grid <- expand.grid(p, q, P, Q)
+names(arima_grid) <- c("p", "q", "P", "Q")
+arima_grid$d <- 1
+arima_grid$D <- 1
+arima_grid$k <- rowSums(arima_grid)
+arima_grid <- arima_grid |> filter(k <= 7)
+
+arima_search <- lapply(1:nrow(arima_grid), function(i) {
+  tryCatch(
+    {
+      md <- arima(
+        train,
+        order = c(arima_grid$p[i], 1, arima_grid$q[i]),
+        seasonal = list(order = c(arima_grid$P[i], 1, arima_grid$Q[i]))
+      )
+      data.frame(
+        p = arima_grid$p[i],
+        d = 1,
+        q = arima_grid$q[i],
+        P = arima_grid$P[i],
+        D = 1,
+        Q = arima_grid$Q[i],
+        AIC = md$aic
+      )
+    },
+    error = function(e) {
+      NULL
+    }
+  )
+}) |>
+  bind_rows() |>
+  arrange(AIC)
+
+# %%
+head(arima_search)
+
+# %%
+# Fit the best SARIMA model
+usgas_best_md <- arima(
+  train,
+  order = c(1, 1, 1),
+  seasonal = list(order = c(2, 1, 1))
+)
+
+# The best model according to the table is SARIMA(1, 1, 1) X (2, 1, 1)_12
+
+# %%
+print(usgas_best_md)
+
+# %%
+# Generate forecasts
+usgas_test_fc <- forecast(usgas_best_md, h = 12)
+print(usgas_test_fc)
+
+# %%
+# Evaluate forecast accuracy
+accuracy(usgas_test_fc, test)
+
+# The MAPE is at 3.51 and 3.31 on training and testing set respectively using SARIMA
+
+# %%
+# Plot forecast results
+test_forecast(USgas, forecast.obj = usgas_test_fc, test = test)
+
+# %%
